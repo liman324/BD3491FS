abc80293637163c09c2be41f8794d355a307c4bc
Alexander Liman <liman324@yandex.ru>
2019-11-08 22:19:46 +0600

    Create test.ino

---
+++ test.ino
@@ -0,0 +1,20 @@
+#include <Wire.h> 
+#include <BD3491FS.h>
+BD3491FS bd; 
+
+void setup(){
+   Wire.begin();
+   audio();
+}
+
+void loop(){}
+
+void audio(){
+    bd.setSelect(0);// int 0...7 === A B C D E F INPUT_SHORT INPUT_MUTE
+    bd.setIn_gain(0); // int 0...7 === 0...20 dB    
+    bd.setVol_1(0); // int 0...87 === 0...-87 dB
+    bd.setVol_2(0); // int 0...87 === 0...-87 dB
+    bd.setBass(0); // int -7...0...+7 === -7...+7 dB
+    bd.setTreb(0); // int -7...0...+7 === -7...+7 dB
+    bd.setSurround(1); // int 0...3 === OFF LOW MIDDLE HIGH
+  }
